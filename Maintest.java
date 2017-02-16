a686d2a88769b37834411d3d3179280c3e4e6fec
caoyj <532720298@qq.com>
2017-02-16 16:00:36 +0800

    1

---
+++ Maintest.java
@@ -0,0 +1,15 @@
+package test1;
+import test1.SIT;
+
+public class Maintest {
+	public static void main(String[]args) throws Exception {
+		SitNew sit=new SIT();
+		newsit.login();
+		newsit.testlogin();
+		newsit.neworder1();
+		newsit.neworder2();
+		newsit.neworder3();
+		newsit.neworder4();		 	
+	}
+
+}
